b00ae631d095e6cbdb35d38874f9f4459d0d46a9
Shaun Clark <shaun.m.clark@gmail.com>
2025-04-16 13:41:22 +0000

    Preparing Tomorrow's Exercises

---
+++ First Attempts/Geeks4Geeks/ThreeCommonNumber.py
@@ -0,0 +1,4 @@
+'''Given three sorted arrays in non-decreasing order, print all common elements in non-decreasing order across these arrays. 
+If there are no such elements return an empty array. In this case, the output will be -1.
+
+Note: can you handle the duplicates without using any additional Data Structure?'''
---
+++ First Attempts/LeetCode/PalindromeNumber.py
@@ -0,0 +1,6 @@
+'''Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Follow up: Could you solve it without converting the integer to a string?'''
+
+'''First Impression: to convert to string a x == x[::-1]
+However, Follow-up suggests another method. Also, to keep in mind that -x can never be a palindrome because of the sign'''
