96176f4901c9118c0d0d690e09e7ea6659325260
Shaun Clark <shaun.m.clark@gmail.com>
2025-04-25 11:10:24 +0200

    Update 4. MedianTwArrays.py

--- First Attempts/LeetCode/4. MedianTwArrays.py
+++ First Attempts/LeetCode/4. MedianTwArrays.py
@@ -18,3 +18,11 @@ Example 2:
 Input: nums1 = [1,2], nums2 = [3,4]
 Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.'''
+
+'''First thoughts; To merge the two and sort; I realise that this is not the most optimal for longer arrays
+Having done research, need to use a Binary Search Tree.
+
+length = len(n) + len(m)
+- Take length/2 left-hand elements from list 1,, Same from list 2 (If odd number, take from list 1)
+- Check that max in both lists < min of leftover elements
+- Adjust pointers accordingly and repeat'''
